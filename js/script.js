4afa5a3f54ea596161a6179bbcf39c6838304a6b
Hảo Nguyễn <nguyenphuochao123@gmail.com>
2024-01-19 16:47:32 +0700

    m

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+$(document).ready(function () {
+    $(".checkout input[name=payment]").change(function () {
+        if ($(this).is(':checked')) {
+            $(".checkout .payment-sumary").css('display', 'none');
+            $(this).parent().next().css('display', 'block');
+        }
+    });
+});
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
 }
