11501e5e72677d9aa6049df025adb390df83b243
Hảo Nguyễn <nguyenphuochao123@gmail.com>
2024-01-22 15:36:17 +0700

    m

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+    // Tìm kiếm sản phẩm auto submit
+    $(".top-menu #fa-magnifying-glass").click(function () {
+        $("#search_form").submit();
+    });
     // Checkout select
     $(".checkout input[name=payment]").change(function () {
         if ($(this).is(':checked')) {
@@ -10,6 +14,45 @@ $(document).ready(function () {
     $(".nav-mobile .menu-mobile-parent").click(function () {
         $(this).parent().next('.menu-mobile-child').toggle();
         $(this).toggleClass("fa-angle-down fa-angle-up");
+    });
+    // Thumbnail image 
+    $(".detail .thumbnail-image img").click(function () {
+        $(".detail .thumbnail-image img").removeClass('active');
+        var thumb_img = $(this).attr('src');
+        $(".detail .big-img img").attr('src', thumb_img);
+        $(this).addClass('active');
+    });
+    // Slick Thumbnail image 
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 });
 function openNav() {
