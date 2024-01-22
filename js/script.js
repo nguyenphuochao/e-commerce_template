2d1de112815fec951de02f6041627afeea26ce49
Hảo Nguyễn <nguyenphuochao123@gmail.com>
2024-01-22 11:25:44 +0700

    m

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 $(document).ready(function () {
+    // Checkout select
     $(".checkout input[name=payment]").change(function () {
         if ($(this).is(':checked')) {
             $(".checkout .payment-sumary").css('display', 'none');
             $(this).parent().next().css('display', 'block');
         }
+    });
+    // Menu mobile
+    $(".nav-mobile .menu-mobile-parent").click(function () {
+        $(this).parent().next('.menu-mobile-child').toggle();
+        $(this).toggleClass("fa-angle-down fa-angle-up");
     });
 });
 function openNav() {
